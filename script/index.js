e77d8b979df958afafa666649ba3080baead8b21
xuedesign <elmo@xuedesign.tw>
2018-12-03 18:11:06 +0800

    頁面樣版建立10% #20181203

---
+++ script/index.js
@@ -0,0 +1,28 @@
+
+//wow載入動畫
+loadjs('script/lib/wow.min.js', function() {
+  new WOW().init();
+});
+
+//圖片延遲載入
+loadjs('https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js', function() {
+    const observer = lozad();
+    observer.observe();
+//});
+
+
+//chatcode
+loadjs('script/lib/chatcode.js');
+
+// 另開視窗
+function externalLinks() { 
+ if (!document.getElementsByTagName) return; 
+ var anchors = document.getElementsByTagName("a"); 
+ for (var i=0; i<anchors.length; i++) { 
+   var anchor = anchors[i]; 
+   if (anchor.getAttribute("href") && 
+       anchor.getAttribute("rel") == "external") 
+     anchor.target = "_blank"; 
+ } 
+} 
+window.onload = externalLinks;
