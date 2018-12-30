7c46d7f35d487c615ced5f0e5dae543327980d7c
xuedesign <elmo@xuedesign.tw>
2018-12-30 16:12:59 +0800

    內頁桌面版製作完成 #20181230

--- script/lib/chatcode.js
+++ script/lib/chatcode.js
@@ -6,7 +6,7 @@
             email: "jiahe.hpj@gmail.com", // Email
             call: "0928-334-522", // Call phone number
             company_logo_url: "//www.jiahe-hpj.com/images/logo.jpg", // URL of company logo (png, jpg, gif)
-            greeting_message: "你好! 歡迎來到嘉禾生技\n需要協助嗎? 發訊息給我們吧", // Text of greeting message
+            greeting_message: "你好! 歡迎來到單細胞策略\n需要協助嗎? 發訊息給我們吧", // Text of greeting message
             button_color: "#E4C273", // Color of button #129BF4 #12D1C7
             position: "right", // Position may be 'right' or 'left'
             order: "facebook,line,call,email" // Order of buttons
