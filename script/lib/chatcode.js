827720306955ebcbdb4ae4df056c035fd8649f9f
xuedesign <elmo@xuedesign.tw>
2020-06-18 11:25:13 +0800

    更新 Chatcode ，修改歡迎詞重複出現問題 #20200618

--- script/lib/chatcode.js
+++ script/lib/chatcode.js
@@ -1,17 +1,14 @@
-//網站多重聯絡方式  WhatsHelp.io widget 
- (function () {
-        var options = {
+       (function () {
+            var options = {
             facebook: "783553025052670", // Facebook page ID
             line: "//line.me/R/ti/p/%40fij8467d", // Line QR code URL
-            email: "jiahe.hpj@gmail.com", // Email
-            call: "0928-334-522", // Call phone number
-            company_logo_url: "//www.jiahe-hpj.com/images/logo.jpg", // URL of company logo (png, jpg, gif)
-            greeting_message: "你好! 歡迎來到單細胞策略\n需要協助嗎? 發訊息給我們吧", // Text of greeting message
-            button_color: "#E4C273", // Color of button #129BF4 #12D1C7
+            //call_to_action: "聯絡我", // Call to action
+            button_color: "#E4C273", // Color of button
             position: "right", // Position may be 'right' or 'left'
-            order: "facebook,line,call,email" // Order of buttons
-        };
-        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+            order: "facebook,line", // Order of buttons
+            };
+
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
         var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
         s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
         var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
